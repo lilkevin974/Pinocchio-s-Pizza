2058624d02014275ab36406faa556f77b1cc7fa5
Kévin ROBERT <lilkevin@hotmail.fr>
2020-06-18 21:54:00 +0200

    Created Pizza model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizza(models.Model):
+    code=models.CharField(max_length=3)
+    pizza=models.CharField(max_length=10)
+    size=models.CharField(max_length=5)
+    toppings=models.IntegerField()
+    price=models.DecimalField(max_digits=4,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.size} {self.pizza} Pizza with {self.toppings} toppings ({self.code}) costs {self.price}$"
